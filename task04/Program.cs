6b9e499b97e53ca7c3e55b82c99d0578ae137ccb
Peter <renemebel@yandex.ru>
2022-11-20 19:00:21 +0300

    задача 4 все четные числа

---
+++ task04/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает 
+все чётные числа от 1 до N.*/
+int userNum = new int();
+Console.WriteLine("Введите число");
+userNum = Convert.ToInt32(Console.ReadLine());
+int StartPos = 0;
+
+while (StartPos < userNum)
+{
+Console.WriteLine(StartPos);
+StartPos = StartPos + 2;
+}
