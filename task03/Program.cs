12280392e9f16e5682d5231924b4785625605d58
Peter <renemebel@yandex.ru>
2022-11-20 18:56:39 +0300

    задача3 делится ли число без остатка

---
+++ task03/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два 
+без остатка)*/
+int userNum = new int();
+Console.WriteLine("Введите число");
+userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum % 2 == 0)
+{
+Console.WriteLine("да");
+}
+else
+{
+Console.WriteLine("нет");
+}
