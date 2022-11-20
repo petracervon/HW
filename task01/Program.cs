661c4905000f4b195a5f7d05519e08a9b893d97d
Peter <renemebel@yandex.ru>
2022-11-20 14:53:42 +0300

    задача первая - найти макс

---
+++ task01/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+int userNum1 = new int();
+Console.WriteLine("Введите число");
+userNum1 = Convert.ToInt32(Console.ReadLine());
+int userNum2 = new int();
+Console.WriteLine("Введите число");
+userNum2 = Convert.ToInt32(Console.ReadLine());
+if (userNum1 < userNum2)
+{
+ Console.WriteLine("max " + userNum2);
+Console.WriteLine("min " + userNum1);
+}
+else 
+{
+ Console.WriteLine("max " + userNum1);
+Console.WriteLine("min " + userNum2);
+}
