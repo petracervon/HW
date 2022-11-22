d9f6bce1327f7863e3b03763bc4977fd63514621
Peter <renemebel@yandex.ru>
2022-11-22 17:31:53 +0300

    second digit

---
+++ task20/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+вторую цифру этого числа.*/
+int userNum = new int();
+Console.WriteLine("Введите число");
+userNum = Convert.ToInt32(Console.ReadLine());
+int Twonumers = userNum / 10;
+int secondDigit = Twonumers % 10;
+
+Console.WriteLine(secondDigit);
