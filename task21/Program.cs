69f377a6f83f7ac0f9a7cb8126db7157b958f2c6
Peter <renemebel@yandex.ru>
2022-11-22 17:52:12 +0300

    WeekEnd

---
+++ task21/Program.cs
@@ -0,0 +1,12 @@
+﻿ //Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int userNum = new int();
+Console.WriteLine("Введите число");
+userNum = Convert.ToInt32(Console.ReadLine());
+
+if (userNum == 6 | userNum == 7) 
+{
+    Console.WriteLine("Выходной");
+}
+else
+
+Console.WriteLine("");
