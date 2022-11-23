69bf043827c6b1fce30acfaa236b47c6d6cb1d2d
Peter <renemebel@yandex.ru>
2022-11-23 20:44:53 +0300

    кубы чисел от 1 до N

---
+++ ttask32/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+int userNum = new int();
+Console.WriteLine("Введите число");
+userNum = Convert.ToInt32(Console.ReadLine());
+double kub = 0;
+double i = 1;
+for (i = 0; i < userNum; i = i+1)
+{
+kub = Math.Pow(i,3);
+Console.Write(kub + " ");
+}
