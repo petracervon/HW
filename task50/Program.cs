3431ca231be04e431b10d1bcb231d1addc607fc0
Peter <renemebel@yandex.ru>
2022-11-26 20:26:41 +0300

    кол-во положительгых эл-ов в массиве

---
+++ task50/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] GenerateRandomArray(int length,int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint+1);
+    }
+    return resultArray;
+}
+int getPositiveElementInArray(int[] array)
+{
+    int quant = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            quant +=1;
+        }
+    }
+    return quant;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+int[] array = GenerateRandomArray(10,100,999);
+printArray(array);
+int sumOfPositive = getPositiveElementInArray((array));
+Console.WriteLine($"Количество чётных элементов {sumOfPositive}");
