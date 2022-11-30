23527955acfb3ef54020060bf382218642c4065e
Peter <renemebel@yandex.ru>
2022-11-30 16:18:37 +0300

    Числа больше 0

--- task60/Program.cs
+++ task60/Program.cs
@@ -1,27 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 0)
-    {
-        count++;
-    }
-}
- 
-Console.WriteLine($"Кол-во элементов > 0: {count}");*/
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите колличество чисел: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-for (int i = 0; i < n; i++)
-{
-    Console.Write($"Введите число {i + 1}: ");
-    int num = Convert.ToInt32(Console.ReadLine());;
-    if (num > 0)
-        count++;
-}
-Console.WriteLine($"чисел > 0: {count}");
+int [] UserNumArray (int length, int start, int end)
+            {
+                Console.Write("Введите число М: ");
 
+                length = Convert.ToInt32(Console.ReadLine());
+                int [] array = new int [length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"элемент массива {i}: ");
+                    array[i] = Convert.ToInt32(Console.ReadLine());                   
+                }
+                return array;
+                
+            }
+            /*void PrintArray(int [] array)
+            {
+                Console.Write("[");
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                    if (i < array.Length - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                }
+                Console.WriteLine("]");
+            }*/
+
+            int NumberOfPositiv (int [] array)
+            {
+                int count = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] > 0)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            int [] newArray = UserNumArray (5,-10, 10);
+            Console.WriteLine();
+            //PrintArray(newArray);
+            int countNumber = NumberOfPositiv(newArray);
+            Console.WriteLine();
+            Console.WriteLine ($"чисел больше нуля {countNumber}");
+       
