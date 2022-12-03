835dcd05f096bc35420e4564a521385cb12df2bd
Peter <renemebel@yandex.ru>
2022-12-03 21:55:26 +0300

    трёхмерный массив с позициями

---
+++ task83/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
+{
+    int[,,] matrix = new int[row, col, dep];
+    Random rnd = new Random();   
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                {
+                    matrix[i,j,k] = rnd.Next(min, max + 1);
+                }
+        }
+    }
+    return matrix;
+} 
+
+ void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(" ");
+            for (int k = 0; k < matrix.GetLength(2); k++) { Console.Write($"{matrix[i, j, k],1}({i},{j},{k}) "); }
+            Console.WriteLine();
+        }
+    }
+}
+PrintMatrix(CreateMatrix(2, 2, 3, 0, 100));
